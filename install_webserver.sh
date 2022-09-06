200bf8557d516f7407b11cf4fc9958cb074e6da4
Kostiantyn <konstantinbelenko36@gmail.com>
2022-09-06 11:40:27 +0200

    changed gitignore

--- install_webserver.sh
+++ install_webserver.sh
@@ -5,13 +5,10 @@ sudo apt-get update && sudo apt-get install apache2
 sudo systemctl enable apache2
 sudo systemctl start apache2
 
-# Build the website and put it inside /var/www/html/
+# Move the website inside /var/www/html/
 
 # Clear the directory
 sudo rm /var/www/html/*
-
-# Build the website
-npm run build
 
 # Copy the build folder insides to the /var/www/html
 sudo mv ./build/* /var/www/html/
